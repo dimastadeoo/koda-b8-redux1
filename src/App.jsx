279704956a79097d161eb31168193615e00372e7
dimastadeoo <dimastadeoo@gmail.com>
2026-06-15 21:49:17 +0700

    feat: create page routes for input forms and response tables

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
-function App(){
-  return (
+import { createBrowserRouter, RouterProvider } from 'react-router';
+import SurveyForm from './component/FormSurvey';
+import TableResponse from './component/TableResponse';
 
-    <>
-      
-    </>
-  )
-   
+
+// Konfigurasi Peta Rute URL halaman
+const router = createBrowserRouter([
+
+  {
+    path: "/",
+    element: <SurveyForm />,
+  },
+  {
+    path: "/response",
+    element: <TableResponse />,
+  }
+])
+
+function App() {
+  // Jalankan konfigurasi router ke dalam aplikasi React
+  return <RouterProvider router={router} />;
 }
 
 export default App;
